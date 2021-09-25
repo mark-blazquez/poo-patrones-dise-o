b5dbde7de62ed388c7830a8f722111c2386f00e3
mark <59871561+mark-blazquez@users.noreply.github.com>
2021-09-25 19:43:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>lista de ejercicios</title>
+</head>
+<body>
+    <h2>ir al index del proyecto</h2>
+    <a href="./vista/index.php">supermercado asir</br>
+</body>
+</html>
